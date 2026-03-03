85e61080812a1b8b04c76d620bd4bb8a2749cb13
taiyoo1104-beep <taiyoo1104@gmail.com>
2026-03-03 10:55:19 +0900

    完成v2 404修正

--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -1,15 +1,13 @@
-import { FileQuestion } from "lucide-react"
 import { memo, type FC } from "react"
 import { Link } from "react-router-dom";
 
 export const Page404:FC = memo(() => {
   return (
     <>
-<div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <FileQuestion size={100} color="#ccc" />
+<div style={{ textAlign: 'center', margin:"0"  }}>
       <h2>ページが見つかりません</h2>
       <p>お探しのページは削除されたか、URLが間違っている可能性があります。</p>
-      <Link to="/login" style={{ color: '#3b82f6', textDecoration: 'underline' }}>
+      <Link to="/login" style={{ color: '#3b82f6', textDecoration: 'underline'}}>
         ログインに戻る
       </Link>
     </div>  </>
